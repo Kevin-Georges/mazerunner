a6a056116d3c799202b5221816a044582f1da44f
K.M.Georges <kmg1g25@srv02094.soton.ac.uk>
2025-11-12 15:16:04 +0000

    Initial commit

---
+++ basic/runner.py
@@ -0,0 +1,52 @@
+#create runner
+def create_runner(x: int = 0, y: int = 0, orientation: str = "N") -> dict:
+    global runner
+    runner = {
+        "x": x,
+        "y": y,
+        "orientation": orientation
+    }
+    return runner
+
+# return values held in dictionary
+def get_x(runner):
+    return runner.get("x")
+
+def get_y(runner):
+    return runner.get("y")
+
+def get_orientation(runner):
+    return runner.get("orientation")
+
+#turn the runner
+def turn(runner, direction: str):
+    #dictionary of each orientation change
+    left_turn = {
+        "N": "W",
+        "W": "S", 
+        "S": "E",
+        "E": "N"
+    }
+    right_turn = {
+        "N": "E",
+        "E": "S",
+        "S": "W",
+        "W": "N"
+    }
+    if direction="Left":
+        runner["orientation"] = left_turn[runner["orientation"]
+    elif direction="Right":
+        runner["orientation"] = right_turn[runner["orientation"]
+    return runner
+
+#move runner forward
+def forward(runner):
+    if get_orientation(runner) == "N":
+        runner["y"] = y+1
+    elif get_orientation(runner) == "E":
+        runner["x"] = x+1
+    elif get_orientation(runner) == "S":
+        runner["y"] = y-1
+    elif get_orientation(runner) == "W":
+        runner["x"] = x-1
+    return runner
